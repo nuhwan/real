e8dd01a83764b783c7ab6a34b1d597da1a01eb3c
nuhwan <guilppang7689@gmail.com>
2024-06-16 23:31:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ from flask import Flask, request, jsonify, render_template
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
+import os
+import json
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
+# 환경 변수 로드
+load_dotenv()
+
 # 구글 시트 인증 설정
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("C:/Users/guilP/attendance_app/service_account.json", scope)
+credentials_info = json.loads(os.getenv('GOOGLE_CREDENTIALS'))
+creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_info, scope)
 client = gspread.authorize(creds)
 sheet = client.open("sheet").sheet1  # 스프레드시트 이름을 정확히 지정
 
@@ -36,4 +43,4 @@ def attendance():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
